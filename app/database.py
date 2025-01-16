ff42b8cb5d868c9073aab9017e1a331793fc3817
AMAN GUPTA <138698147+amangupta7456@users.noreply.github.com>
2025-01-16 13:31:39 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 
 # MongoDB setup
-MONGODB_URL = "mongodb+srv://amangupta1708aj:w7xSA2oeIbX6rj0U@cluster0.7qmey.mongodb.net/client_config_db"  
+MONGODB_URL = "mongodb+srv://amangupta1708aj:password/client_config_db"  
 DATABASE_NAME = "client_config_db"
 COLLECTION_NAME = "client_configurations"
 
